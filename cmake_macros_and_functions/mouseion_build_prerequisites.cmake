6fc73b976987c41b0f5a5f9936bc236efbfa58a4
Arnold Tharringtom <arnoldt@ornl.gov>
2021-01-09 10:14:49 -0500

    After lunch update.

--- cmake_macros_and_functions/mouseion_build_prerequisites.cmake
+++ cmake_macros_and_functions/mouseion_build_prerequisites.cmake
@@ -7,90 +7,91 @@
 function(verify_key_environmental_are_set)
 
     #-----------------------------------------------------
-    # Verify environmental variable ANANSI_TOP_LEVEL is  -
+    # Verify environmental variable MOUSEION_TOP_LEVEL is  -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED ENV{ANANSI_TOP_LEVEL})
-        message("ANANSI_TOP_LEVEL=$ENV{ANANSI_TOP_LEVEL}")
+    if(DEFINED ENV{MOUSEION_TOP_LEVEL})
+        message("MOUSEION_TOP_LEVEL=$ENV{MOUSEION_TOP_LEVEL}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_TOP_LEVEL is not defined. This \
-        variable defines the top level directory of the anansi package." )
+        message( FATAL_ERROR "The environmental MOUSEION_TOP_LEVEL is not defined. This \
+        variable defines the top level directory of the mouseion package." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # ANANSI_CMAKE_CXX_COMPILER is defined.              -
+    # MOUSEION_CMAKE_CXX_COMPILER is defined.              -
     #-----------------------------------------------------
-    if (DEFINED ENV{ANANSI_CMAKE_CXX_COMPILER})
-        message("ANANSI_CMAKE_CXX_COMPILER=$ENV{ANANSI_CMAKE_CXX_COMPILER}")
+    if (DEFINED ENV{MOUSEION_CMAKE_CXX_COMPILER})
+        message("MOUSEION_CMAKE_CXX_COMPILER=$ENV{MOUSEION_CMAKE_CXX_COMPILER}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_CMAKE_CXX_COMPILER is not defined. This \
-        variable defines the C++ compiler for compiling anansi." )
+        message( FATAL_ERROR "The environmental MOUSEION_CMAKE_CXX_COMPILER is not defined. This \
+        variable defines the C++ compiler for compiling mouseion." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # ANANSI_CMAKE_C_COMPILER is defined.                -
+    # MOUSEION_CMAKE_C_COMPILER is defined.                -
     #-----------------------------------------------------
-    if (DEFINED ENV{ANANSI_CMAKE_C_COMPILER})
-        message("ANANSI_CMAKE_C_COMPILER=$ENV{ANANSI_CMAKE_C_COMPILER}")
+    if (DEFINED ENV{MOUSEION_CMAKE_C_COMPILER})
+        message("MOUSEION_CMAKE_C_COMPILER=$ENV{MOUSEION_CMAKE_C_COMPILER}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_CMAKE_C_COMPILER is not defined. This \
-        variable defines the c compiler for compiling anansi." )
-    endif()
-
-    #-----------------------------------------------------
-    # Verify environmental variable ANANSI_INSTALL_PREFIX-
-    # is defined.                                        -
-    #-----------------------------------------------------
-    if (DEFINED ENV{ANANSI_INSTALL_PREFIX})
-        message("ANANSI_INSTALL_PREFIX=$ENV{ANANSI_INSTALL_PREFIX}")
-    else()
-        message( FATAL_ERROR "The environmental ANANSI_INSTALL_PREFIX is not defined. This \
-        variable defines the root location to install anansi." )
+        message( FATAL_ERROR "The environmental MOUSEION_CMAKE_C_COMPILER is not defined. This \
+        variable defines the c compiler for compiling mouseion." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # ANANSI_INSTALL_BIN_DIRECTORY is defined.           -
+    # MOUSEION_INSTALL_PREFIX  is defined.               -
+    #                                                    -
     #-----------------------------------------------------
-    if (DEFINED ENV{ANANSI_INSTALL_BIN_DIRECTORY})
-        message("ANANSI_INSTALL_BIN_DIRECTORY=$ENV{ANANSI_INSTALL_BIN_DIRECTORY}")
+    if (DEFINED ENV{MOUSEION_INSTALL_PREFIX})
+        message("MOUSEION_INSTALL_PREFIX=$ENV{MOUSEION_INSTALL_PREFIX}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_INSTALL_BIN_DIRECTORY is not defined. This \
-        variable defines the location to install anansi binaries." )
+        message( FATAL_ERROR "The environmental MOUSEION_INSTALL_PREFIX is not defined. This \
+        variable defines the root location to install mouseion." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # ANANSI_INSTALL_INCLUDE_DIRECTORY is defined.       -
+    # MOUSEION_INSTALL_BIN_DIRECTORY is defined.         -
     #-----------------------------------------------------
-    if ( DEFINED ENV{ANANSI_INSTALL_INCLUDE_DIRECTORY} )
-        message("ANANSI_INSTALL_INCLUDE_DIRECTORY=$ENV{ANANSI_INSTALL_INCLUDE_DIRECTORY}")
+    if (DEFINED ENV{MOUSEION_INSTALL_BIN_DIRECTORY})
+        message("MOUSEION_INSTALL_BIN_DIRECTORY=$ENV{MOUSEION_INSTALL_BIN_DIRECTORY}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_INSTALL_INCLUDE_DIRECTORY is not defined. This \
-        variable defines the location to install anansi header files." )
+        message( FATAL_ERROR "The environmental MOUSEION_INSTALL_BIN_DIRECTORY is not defined. This \
+        variable defines the location to install mouseion binaries." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # ANANSI_INSTALL_LIB_DIRECTORY is defined.           -
+    # MOUSEION_INSTALL_INCLUDE_DIRECTORY is defined.     -
     #-----------------------------------------------------
-    if ( DEFINED ENV{ANANSI_INSTALL_LIB_DIRECTORY} )
-        message("ANANSI_INSTALL_LIB_DIRECTORY=$ENV{ANANSI_INSTALL_LIB_DIRECTORY}")
+    if ( DEFINED ENV{MOUSEION_INSTALL_INCLUDE_DIRECTORY} )
+        message("MOUSEION_INSTALL_INCLUDE_DIRECTORY=$ENV{MOUSEION_INSTALL_INCLUDE_DIRECTORY}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_INSTALL_LIB_DIRECTORY is not defined. This \
-        variable defines the location to install anansi libraries." )
+        message( FATAL_ERROR "The environmental MOUSEION_INSTALL_INCLUDE_DIRECTORY is not defined. This \
+        variable defines the location to install mouseion header files." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable                      -
+    # MOUSEION_INSTALL_LIB_DIRECTORY is defined.         -
+    #-----------------------------------------------------
+    if ( DEFINED ENV{MOUSEION_INSTALL_LIB_DIRECTORY} )
+        message("MOUSEION_INSTALL_LIB_DIRECTORY=$ENV{MOUSEION_INSTALL_LIB_DIRECTORY}")
+    else()
+        message( FATAL_ERROR "The environmental MOUSEION_INSTALL_LIB_DIRECTORY is not defined. This \
+        variable defines the location to install mouseion libraries." )
     endif()
 
     #-----------------------------------------------------
     # Verify environment variable                        -
-    # ANANSI_BOOST_TOP_LEVEL is defined.                 -
+    # MOUSEION_BOOST_TOP_LEVEL is defined.               -
     #-----------------------------------------------------
-    if( DEFINED ENV{ANANSI_BOOST_TOP_LEVEL})
-        message("ANANSI_BOOST_TOP_LEVEL=$ENV{ANANSI_BOOST_TOP_LEVEL}")
+    if( DEFINED ENV{MOUSEION_BOOST_TOP_LEVEL})
+        message("MOUSEION_BOOST_TOP_LEVEL=$ENV{MOUSEION_BOOST_TOP_LEVEL}")
     else()
-        message( FATAL_ERROR "The environmental ANANSI_BOOST_TOP_LEVEL is not defined. This \
+        message( FATAL_ERROR "The environmental MOUSEION_BOOST_TOP_LEVEL is not defined. This \
         variable defines the location to BOOST top level." )
     endif()
 
