74443bb314dc096f1deab84e3432fd8dff15ea34
Arnold Tharrington <arnoldt@ornl.gov>
2022-03-29 14:26:20 -0400

    Added more using namespace alias that target the boost mp11 library.

--- sources/meta_programming_library/header_files/MPLAliases.hpp
+++ sources/meta_programming_library/header_files/MPLAliases.hpp
@@ -40,6 +40,18 @@ using mpl_typelist = boost::mp11::mp_list<T...>;
 template<typename T>
 using mpl_type2type = boost::mp11::mp_identity<T>;
 
+// ----------------------------------------------------
+// Wraps a static constant of type bool.
+// ----------------------------------------------------
+template <bool T>
+using mpl_bool = boost::mp11::mp_bool<T>;
+
+// ----------------------------------------------------
+// Same as std::bool_const in C++17
+// Wraps the static constant true type.
+// ----------------------------------------------------
+using mpl_true_type = boost::mp11::mp_bool<true>;
+
 //-----------------------------------------------------
 // The alias to boost mp11 list size
 //-----------------------------------------------------
@@ -49,7 +61,7 @@ using mpl_size = boost::mp11::mp_size<L>;
 //-----------------------------------------------------
 // The alias to boost mp11 list size 0.
 //-----------------------------------------------------
-using mpl_size_0 = mpl_size< mpl_typelist<>>; 
+using mpl_size_0 = mpl_size<mpl_typelist<>>; 
 
 //-----------------------------------------------------
 // The alias to boost mp11 list size 1.
@@ -100,14 +112,36 @@ using mpl_reverse = boost::mp11::mp_reverse<L>;
 template <typename L,std::size_t I>
 using mpl_at_c = boost::mp11::mp_at_c<L,I>;
 
-template<typename L, template <class...> typename P >
-using mpl_find_if = boost::mp11::mp_find_if<L,P>;
-
-template<typename L, typename Q >
-using mpl_find_if_q = boost::mp11::mp_find_if_q<L,Q>;
-
+// ----------------------------------------------------
+// If Derived is derived from Base or if both are the same non-union class (in
+// both cases ignoring cv-qualification), provides the member constant value
+// equal to true. Otherwise value is false.
+// ----------------------------------------------------
 template<class Base,class Derived>
-using M_mpl_is_base_of = std::is_base_of<Base,Derived>;
+using mpl_is_base_of = std::is_base_of<Base,Derived>;
+
+// ----------------------------------------------------
+// Returns a list of the same form as L that consists of N concatenated copies
+// of L.
+// ----------------------------------------------------
+template <typename L, std::size_t N>
+using mpl_repeat_c = boost::mp11::mp_repeat_c<L,N>;
+
+// ----------------------------------------------------
+// mpl_transform<F, L1<T1…>, L2<T2…>, …, Ln<Tn…>> applies F to each successive
+// tuple of elements and returns L1<F<T1, T2, …, Tn>…>
+// ----------------------------------------------------
+template<template<class...> class F, class... L> 
+using mpl_transform = boost::mp11::mp_transform<F,L...>;
+
+
+// ----------------------------------------------------
+// mpl_find<L, V> returns the index at which the type V is located in the list
+// L. If L does not contain V, mpl_find<L, V> is
+// mpl_size<L>.
+// ----------------------------------------------------
+template<typename L, typename V>
+using mpl_find = boost::mp11::mp_find<L,V>;
 
 }; // namespace MPL
 
