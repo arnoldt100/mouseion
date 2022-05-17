db7a59ecd4c5d6e350cfefea95579219c51b7386
Arnold Tharrington <arnoldt@ornl.gov>
2022-05-17 12:08:34 -0400

    Initial tracking of MPIEnvironmentState.(.h,cpp).
    Initial addition of class MPIEnvironmentState.

---
+++ sources/communicator/header_files/MPIEnvironmentState.h
@@ -0,0 +1,70 @@
+#ifndef  COMMUNICATOR_MPIEnvironmentState_INC
+#define  COMMUNICATOR_MPIEnvironmentState_INC
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace COMMUNICATOR
+{
+
+// =====================================================================================
+//        Class:  MPIEnvironmentState
+//  Description:  
+//  =====================================================================================
+class MPIEnvironmentState
+{
+    public:
+        // ====================  LIFECYCLE     =======================================
+
+        //--------------------------------------------------------------------------------------
+        //       Class:  MPIEnvironmentState
+        //      Method:  MPIEnvironmentState :: MPIEnvironmentState
+        // Description:  
+        // 
+        //  Parameters: 
+        //
+        //      Return:
+        //--------------------------------------------------------------------------------------
+        MPIEnvironmentState ();   // constructor
+
+        MPIEnvironmentState (const MPIEnvironmentState & other);   // copy constructor
+
+        MPIEnvironmentState (MPIEnvironmentState && other);   // copy-move constructor
+
+        virtual ~MPIEnvironmentState ()=0;  // destructor
+
+        // ====================  ACCESSORS     =======================================
+
+        // ====================  MUTATORS      =======================================
+
+        // ====================  OPERATORS     =======================================
+
+        MPIEnvironmentState& operator= ( const MPIEnvironmentState &other ); // assignment operator
+
+        MPIEnvironmentState& operator= ( MPIEnvironmentState && other ); // assignment-move operator
+
+    protected:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+    private:
+        // ====================  METHODS       =======================================
+
+        // ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class MPIEnvironmentState  -----
+
+
+}; // namespace __NAMESPACE__
+
+#endif   /* ----- #ifndef COMMUNICATOR_MPIEnvironmentState_INC  ----- */
