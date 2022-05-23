832af5064af78ceec59e7682a7adb099e5a5e4c0
Arnold Tharrington <arnoldt@ornl.gov>
2022-05-23 14:20:49 -0400

    Changed mpi_communicator_install_include_directory to communicator_install_include_directory.
    In several CMakeLists.txt files the name
    mpi_communicator_install_include_directory was changed to
    communicator_install_include_directory.

--- sources/communicator/cmake_macros_and_functions/communicator_build_prerequisites.cmake
+++ sources/communicator/cmake_macros_and_functions/communicator_build_prerequisites.cmake
@@ -21,14 +21,14 @@ function(verify_key_environmental_are_set)
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # mpi_communicator_install_include_directory         -
+    # communicator_install_include_directory             -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED mpi_communicator_install_include_directory)
-        set(log_message "mpi_communicator_install_include_directory=${mpi_communicator_install_include_directory}\n")
+    if(DEFINED communicator_install_include_directory)
+        set(log_message "communicator_install_include_directory=${communicator_install_include_directory}\n")
         file(APPEND ${mouseion_log_file} "${log_message}")
     else()
-        message( FATAL_ERROR "The variable mpi_communicator_install_include_directory is not defined. This \
+        message( FATAL_ERROR "The variable communicator_install_include_directory is not defined. This \
         variable defines the directory to install the header files for mpi_communicator." )
     endif()
 
