888643a7d2b7ce3199dc30a6afe3937a62df6bc8
Arnold Tharrington <arnoldt@ornl.gov>
2022-06-13 16:34:58 -0400

    Modified foreach to generic_foreach.

--- sources/meta_programming_library/generic_foreach_loop/cmake_macros_and_functions/foreach_loop.cmake
+++ sources/meta_programming_library/generic_foreach_loop/cmake_macros_and_functions/foreach_loop.cmake
@@ -1,6 +1,6 @@
 # =====================================================================================
 # 
-#        Filename:  foreach_loop.cmake
+#        Filename:  generic_foreach_loop.cmake
 # 
 #     Description:  
 # 
@@ -11,9 +11,9 @@
 
 
 # ===  MACRO     ======================================================================
-#         Name:  enable_building_foreach_loop_library
+#         Name:  enable_building_generic_foreach_loop_library
 #  Description:  Defines key variables enabling the
-#                building of the foreach_loop library.
+#                building of the generic_foreach_loop library.
 # 
 #  Arguments: 
 #             install_lib_directory - The location to install the actual library
@@ -23,18 +23,18 @@
 #             cmake_cxx_compiler - The c++ compiler to build the library
 #             cxx_standard - The C++ standard to build the library and binaries
 # =====================================================================================
-macro ( enable_building_foreach_loop_library install_lib_directory
+macro ( enable_building_generic_foreach_loop_library install_lib_directory
         install_include_directory
         install_bin_directory
         cmake_c_compiler 
         cmake_cxx_compiler 
         cxx_standard)
 
-    set(foreach_loop_install_lib_directory ${install_lib_directory})
-    set(foreach_loop_install_include_directory ${install_include_directory})
-    set(foreach_loop_install_bin_directory ${install_bin_directory})
-    set(foreach_loop_cmake_cxx_compiler ${cmake_cxx_compiler})
-    set(foreach_loop_cmake_c_compiler ${cmake_c_compiler})
-    set(foreach_loop_cxx_standard ${cxx_standard})
+    set(generic_foreach_loop_install_lib_directory ${install_lib_directory})
+    set(generic_foreach_loop_install_include_directory ${install_include_directory})
+    set(generic_foreach_loop_install_bin_directory ${install_bin_directory})
+    set(generic_foreach_loop_cmake_cxx_compiler ${cmake_cxx_compiler})
+    set(generic_foreach_loop_cmake_c_compiler ${cmake_c_compiler})
+    set(generic_foreach_loop_cxx_standard ${cxx_standard})
 endmacro()
 
--- sources/meta_programming_library/generic_foreach_loop/cmake_macros_and_functions/foreach_loop_build_prerequisites.cmake
+++ sources/meta_programming_library/generic_foreach_loop/cmake_macros_and_functions/foreach_loop_build_prerequisites.cmake
@@ -9,68 +9,61 @@ function(verify_key_environmental_are_set)
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # foreach_loop_cmake_cxx_compiler is defined.        -
+    # generic_foreach_loop_cmake_cxx_compiler is         -
+    # defined.                                           -
     #-----------------------------------------------------
-    if (DEFINED foreach_loop_cmake_cxx_compiler)
-        message("foreach_loop_cmake_cxx_compiler=${foreach_loop_cmake_cxx_compiler}")
+    if (DEFINED generic_foreach_loop_cmake_cxx_compiler)
+        message("generic_foreach_loop_cmake_cxx_compiler=${generic_foreach_loop_cmake_cxx_compiler}")
     else()
-        message( FATAL_ERROR "The variable foreach_loop_cmake_cxx_compiler is not defined. This \
-        variable defines the C++ compiler for compiling the foreach_loop library." )
+        message( FATAL_ERROR "The variable generic_foreach_loop_cmake_cxx_compiler is not defined. This \
+        variable defines the C++ compiler for compiling the generic_foreach_loop library." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # foreach_loop_cmake_c_compiler is defined.          -
+    # generic_foreach_loop_cmake_c_compiler is defined.  -
     #-----------------------------------------------------
-    if (DEFINED foreach_loop_cmake_c_compiler)
-        message("foreach_loop_cmake_c_compiler=${foreach_loop_cmake_c_compiler}")
+    if (DEFINED generic_foreach_loop_cmake_c_compiler)
+        message("generic_foreach_loop_cmake_c_compiler=${generic_foreach_loop_cmake_c_compiler}")
     else()
-        message( FATAL_ERROR "The variable foreach_loop_cmake_c_compiler is not defined. This \
-        variable defines the c compiler for compiling foreach_loop library." )
+        message( FATAL_ERROR "The variable generic_foreach_loop_cmake_c_compiler is not defined. This \
+        variable defines the c compiler for compiling generic_foreach_loop library." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # foreach_loop_install_bin_directory is defined.     -
+    # generic_foreach_loop_install_bin_directory is      -
+    # defined.                                           -
     #-----------------------------------------------------
-    if (DEFINED foreach_loop_install_bin_directory)
-        message("foreach_loop_install_bin_directory=${foreach_loop_install_bin_directory}")
+    if (DEFINED generic_foreach_loop_install_bin_directory)
+        message("generic_foreach_loop_install_bin_directory=${generic_foreach_loop_install_bin_directory}")
     else()
-        message( FATAL_ERROR "The variable foreach_loop_install_bin_directory is not defined. This \
-        variable defines the location to install foreach_loop binaries." )
+        message( FATAL_ERROR "The variable generic_foreach_loop_install_bin_directory is not defined. This \
+        variable defines the location to install generic_foreach_loop binaries." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # foreach_loop_install_include_directory is defined. -
+    # generic_foreach_loop_install_include_directory is  -
+    # defined.                                           -
     #-----------------------------------------------------
-    if ( DEFINED foreach_loop_install_include_directory )
-        message("foreach_loop_install_include_directory=${foreach_loop_install_include_directory}")
+    if ( DEFINED generic_foreach_loop_install_include_directory )
+        message("generic_foreach_loop_install_include_directory=${generic_foreach_loop_install_include_directory}")
     else()
-        message( FATAL_ERROR "The variable foreach_loop_install_include_directory is not defined. This \
-        variable defines the location to install foreach_loop header files." )
+        message( FATAL_ERROR "The variable generic_foreach_loop_install_include_directory is not defined. This \
+        variable defines the location to install generic_foreach_loop header files." )
     endif()
 
     #-----------------------------------------------------
     # Verify environmental variable                      -
-    # foreach_loop_install_lib_directory is defined.     -
+    # generic_foreach_loop_install_lib_directory is      -
+    # defined.                                           -
     #-----------------------------------------------------
-    if ( DEFINED foreach_loop_install_lib_directory )
-        message("foreach_loop_install_lib_directory=${foreach_loop_install_lib_directory}")
+    if ( DEFINED generic_foreach_loop_install_lib_directory )
+        message("generic_foreach_loop_install_lib_directory=${generic_foreach_loop_install_lib_directory}")
     else()
-        message( FATAL_ERROR "The variable foreach_loop_install_lib_directory is not defined. This \
-        variable defines the location to install foreach_loop libraries." )
-    endif()
-
-    #-----------------------------------------------------
-    # Verify environment variable                        -
-    # foreach_loop_boost_top_level is defined.           -
-    #-----------------------------------------------------
-    if( DEFINED ENV{foreach_loop_boost_top_level})
-        message("foreach_loop_boost_top_level=${foreach_loop_boost_top_level}")
-    else()
-        message( FATAL_ERROR "The variable foreach_loop_boost_top_level is not defined. This \
-        variable defines the location to BOOST top level." )
+        message( FATAL_ERROR "The variable generic_foreach_loop_install_lib_directory is not defined. This \
+        variable defines the location to install generic_foreach_loop libraries." )
     endif()
 
 endfunction()
