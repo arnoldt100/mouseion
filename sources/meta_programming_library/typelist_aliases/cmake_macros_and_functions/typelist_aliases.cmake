48fe1c74ba2c6c15406a613ed61f95f76fbb600b
Arnold Tharrington <arnoldt@ornl.gov>
2022-06-14 13:08:32 -0400

    Nintial tracking of files in package typelist_aliases.

---
+++ sources/meta_programming_library/typelist_aliases/cmake_macros_and_functions/typelist_aliases.cmake
@@ -0,0 +1,45 @@
+# =====================================================================================
+# 
+#        Filename:  typelist_aliases.cmake
+# 
+#     Description:  
+# 
+#          Author:  Arnold N. Tharrington (), arnoldt@ornl.gov
+#    Organization:  ORNL-National Center of Computational Sciences
+# 
+#  =====================================================================================
+
+
+# ===  MACRO     ======================================================================
+#         Name:  enable_building_typelist_aliases_library
+#  Description:  Defines key variables enabling the
+#                building of the typelist_aliases library.
+# 
+#  Arguments: top_level - The top level of the main CmakeLists.txt file.
+#             minimum_cmake_version - The minumum cmake version for building typelist_aliases
+#             cmake_c_compiler - The C compiler for building c sources
+#             cmake_cxx_compiler - The C++ compiler for building C++ sources
+#             install_prefix - The root location to install the library.
+#             install_bin_directory - The location to install binaries
+#             install_include_directory - The location to install header files
+#             install_lib_directory - The location to install the actual library
+#             boost_top_level - The top level of the boost package
+# =====================================================================================
+macro (enable_building_typelist_aliases_library minimum_cmake_version
+       cxx_standard
+       cmake_c_compiler 
+       cmake_cxx_compiler
+       install_bin_directory
+       install_include_directory
+       install_lib_directory
+       boost_top_level)
+
+    set(typelist_aliases_minimum_cmake_version ${minimum_cmake_version}) 
+    set(typelist_aliases_cxx_standard ${cxx_standard})
+    set(typelist_aliases_cmake_c_compiler ${cmake_c_compiler})
+    set(typelist_aliases_cmake_cxx_compiler ${cmake_cxx_compiler})
+    set(typelist_aliases_install_bin_directory ${install_bin_directory}) 
+    set(typelist_aliases_install_include_directory ${install_include_directory})
+    set(typelist_aliases_install_lib_directory ${install_lib_directory})
+    set(typelist_aliases_boost_top_level ${boost_top_level})
+endmacro()
---
+++ sources/meta_programming_library/typelist_aliases/cmake_macros_and_functions/typelist_aliases_build_prerequisites.cmake
@@ -0,0 +1,98 @@
+#-----------------------------------------------------
+# This function verifies the                         -
+# variables are set.                                 -
+#                                                    -
+#                                                    -
+#-----------------------------------------------------
+function(verify_typelist_aliases_build_prerequisites)
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_minimum_cmake_version is defined.
+    #-----------------------------------------------------
+    if (DEFINED typelist_aliases_minimum_cmake_version)
+        message("typelist_aliases_minimum_cmake_version=${typelist_aliases_minimum_cmake_version}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_minimum_cmake_version is not defined. This \
+        variable defines the minimum cmake version for compiling typelist_aliases." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_cxx_standard is defined.
+    #-----------------------------------------------------
+    if (DEFINED typelist_aliases_cxx_standard)
+        message("typelist_aliases_minimum_cmake_version=${typelist_aliases_cxx_standard}")
+    else(typelist_aliases_cxx_standard
+        message( FATAL_ERROR "The variable typelist_aliases_cxx_standard is not defined. This \
+        variable defines the cxx standard for compiling typelist_aliases." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_cmake_cxx_compiler is defined.
+    #-----------------------------------------------------
+    if (DEFINED typelist_aliases_cmake_cxx_compiler)
+        message("typelist_aliases_cmake_cxx_compiler=${typelist_aliases_cmake_cxx_compiler}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_cmake_cxx_compiler is not defined. This \
+        variable defines the C++ compiler for compiling typelist_aliases." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_cmake_c_compiler is defined.
+    #-----------------------------------------------------
+    if (DEFINED typelist_aliases_cmake_c_compiler)
+        message("typelist_aliases_cmake_c_compiler=${typelist_aliases_cmake_c_compiler}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_cmake_c_compiler is not defined. This \
+        variable defines the c compiler for compiling typelist_aliases." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_install_bin_directory is defined.
+    #-----------------------------------------------------
+    if (DEFINED {typelist_aliases_install_bin_directory})
+        message("typelist_aliases_install_bin_directory=${typelist_aliases_install_bin_directory}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_install_bin_directory is not defined. This \
+        variable defines the location to install typelist_aliases binaries." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_install_include_directory is 
+    # defined.
+    #-----------------------------------------------------
+    if ( DEFINED typelist_aliases_install_include_directory )
+        message("typelist_aliases_install_include_directory=${typelist_aliases_install_include_directory}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_install_include_directory is not defined. This \
+        variable defines the location to install typelist_aliases header files." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environmental variable
+    # typelist_aliases_install_lib_directory is defined.
+    #-----------------------------------------------------
+    if ( DEFINED typelist_aliases_install_lib_directory )
+        message("typelist_aliases_install_lib_directory=${typelist_aliases_install_lib_directory}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_install_lib_directory is not defined. This \
+        variable defines the location to install typelist_aliases libraries." )
+    endif()
+
+    #-----------------------------------------------------
+    # Verify environment variable
+    # typelist_aliases_boost_top_level is defined.
+    #-----------------------------------------------------
+    if( DEFINED typelist_aliases_boost_top_level)
+        message("typelist_aliases_boost_top_level=${typelist_aliases_boost_top_level}")
+    else()
+        message( FATAL_ERROR "The variable typelist_aliases_boost_top_level is not defined. This \
+        variable defines the location to BOOST top level." )
+    endif()
+
+endfunction()
