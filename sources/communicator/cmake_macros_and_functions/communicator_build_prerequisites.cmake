ce7b92a4bab2ca66b8fb585d794b86fa4ffdfcca
Arnold Tharrington <arnoldt@ornl.gov>
2023-04-05 17:36:14 -0400

    Fixed CMake macros and functions for target communicator.

--- sources/communicator/cmake_macros_and_functions/communicator_build_prerequisites.cmake
+++ sources/communicator/cmake_macros_and_functions/communicator_build_prerequisites.cmake
@@ -7,55 +7,53 @@
 function(verify_key_environmental_are_set)
 
     #-----------------------------------------------------
-    # Verify environmental variable                      -
-    # communicator_install_lib_directory             -
+    # Verify  variable                                   -
+    # communicator_install_lib_directory                 -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED communicator_install_lib_directory)
-        set(log_message "communicator_install_lib_directory=${communicator_install_lib_directory}\n")
-        file(APPEND ${mouseion_log_file} "${log_message}")
-    else()
-        message( FATAL_ERROR "The variable communicator_install_lib_directory is not defined. This \
-        variable defines the directory to install the library communicator." )
+   # Ensure that variable "communicator_logfilepath" is defined.
+    if (NOT DEFINED communicator_logfilepath) 
+        message (FATAL_ERROR "communicator_logfilepath is not defined.")
     endif()
+    
+    #-----------------------------------------------------
+    # Verify variable communicator_install_lib_directory defined.
+    #-----------------------------------------------------
+    mouseion_test_variable_is_defined("communicator_install_lib_directory" "${communicator_logfilepath}")
 
     #-----------------------------------------------------
-    # Verify environmental variable                      -
+    # Verify variable                                    -
     # communicator_install_include_directory             -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED communicator_install_include_directory)
-        set(log_message "communicator_install_include_directory=${communicator_install_include_directory}\n")
-        file(APPEND ${mouseion_log_file} "${log_message}")
-    else()
-        message( FATAL_ERROR "The variable communicator_install_include_directory is not defined. This \
-        variable defines the directory to install the header files for communicator." )
-    endif()
+    mouseion_test_variable_is_defined("communicator_install_include_directory" "${communicator_logfilepath}")
 
     #-----------------------------------------------------
-    # Verify environmental variable                      -
+    # Verify variable                                    -
     # communicator_install_bin_directory                 -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED communicator_install_bin_directory)
-        set(log_message "communicator_install_bin_directory=${communicator_install_bin_directory}\n")
-        file(APPEND ${mouseion_log_file} "${log_message}")
-    else()
-        message( FATAL_ERROR "The variable communicator_install_bin_directory is not defined. This \
-        variable defines the directory to install the header files for communicator." )
-    endif()
+    mouseion_test_variable_is_defined("communicator_install_bin_directory" "${communicator_logfilepath}")
 
     #-----------------------------------------------------
-    # Verify environmental variable                      -
+    # Verify variable                                    -
+    # communicator_cmake_cxx_compiler                    -
+    # defined.                                           -
+    #-----------------------------------------------------
+    mouseion_test_variable_is_defined("communicator_cmake_cxx_compiler" "${communicator_logfilepath}")
+
+    #-----------------------------------------------------
+    # Verify variable                                    -
+    # communicator_cmake_c_compiler                      -
+    # defined.                                           -
+    #-----------------------------------------------------
+    mouseion_test_variable_is_defined("communicator_cmake_c_compiler" "${communicator_logfilepath}")
+
+    #-----------------------------------------------------
+    # Verify variable                                    -
     # communicator_cxx_standard                          -
     # defined.                                           -
     #-----------------------------------------------------
-    if(DEFINED communicator_cxx_standard)
-        set(log_message "communicator_cxx_standard=${communicator_cxx_standard}\n")
-        file(APPEND ${mouseion_log_file} "${log_message}")
-    else()
-        message( FATAL_ERROR "The variable communicator_cxx_standard is not defined. This \
-        variable defines the c++ standard." )
-    endif()
+    mouseion_test_variable_is_defined("communicator_cxx_standard" "${communicator_logfilepath}")
 
 endfunction()
