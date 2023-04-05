0fe78da77c86f84c922a007a33923c5462e92da8
Arnold Tharrington <arnoldt@ornl.gov>
2023-04-05 13:24:01 -0400

    Transient commit for target primitives.

---
+++ cmake_macros_and_functions/mouseion_log_message.cmake
@@ -0,0 +1,17 @@
+
+# ===  FUNCTION  ======================================================================
+#         Name:  mouseion_log_message_to_file
+#  Description:  Write a message to file by appending the message to the file.
+# 
+#   Parameters: 
+#               filepath : The name of the file to write the message
+#               message : The message to be logged to file
+#        Return:
+# =====================================================================================
+function ( mouseion_log_message_to_file filepath message )
+
+    string(TIMESTAMP my_date) 
+    set (log_message "${my_date} : ${message}\n")
+    file (APPEND  ${filepath} ${log_message} )
+
+endfunction()
