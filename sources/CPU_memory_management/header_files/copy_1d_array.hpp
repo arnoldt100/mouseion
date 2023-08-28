06238de5b8de5599a313a2f636fcb3c2fbb04c1f
arnoldt <arnoldt@ornl.gov>
2023-08-28 15:01:58 -0400

    Start of workday on Catalan.

---
+++ sources/CPU_memory_management/header_files/copy_1d_array.hpp
@@ -0,0 +1,27 @@
+#ifndef MEMORY_MANAGEMENT_copy_1d_array_INC
+#define MEMORY_MANAGEMENT_copy_1d_array_INC
+
+//! \file copy_1d_array.hpp
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+#include <tuple>
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+namespace MEMORY_MANAGEMENT
+{
+
+//! \brief Stud text for brief description
+void copy_1d_array ();
+
+}; // namespace MEMORY_MANAGEMENT
+
+#endif // MEMORY_MANAGEMENT_copy_1d_array_INC
