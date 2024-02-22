5905081ac903922a607f05647744f8cc30e5b300
Arnold Tharrington <arnoldt@ornl.gov>
2024-02-22 10:25:03 -0500

    Fixed a function signature,
    The function definition didn't match the declaration.

--- sources/string_utilities/implementation_files/split_string_by_delimiter.cpp
+++ sources/string_utilities/implementation_files/split_string_by_delimiter.cpp
@@ -15,12 +15,13 @@
 namespace STRING_UTILITIES
 {
 
-std::vector<std::string> split_string_by_delimiter(  const std::string input )
+std::vector<std::string> split_string_by_delimiter(  const std::string input, 
+                                                     const std::string delimiter)
 {
     std::vector<std::string> my_strings;
     boost::algorithm::split(my_strings,
                             input,
-                            boost::is_any_of(";"),
+                            boost::is_any_of(delimiter.c_str()),
                             boost::token_compress_on);
     return my_strings;
 }   // -----  end of function split_string_by_delimiter  -----
