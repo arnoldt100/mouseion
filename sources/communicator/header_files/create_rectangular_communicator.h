646446b65499ee6be2f5c29347235836c0a27f86
Arnold Tharrington <arnoldt@ornl.gov>
2024-03-13 10:58:24 -0400

    End of workday on RuyLopez.

---
+++ sources/communicator/header_files/create_rectangular_communicator.h
@@ -0,0 +1,28 @@
+#ifndef COMMUNICATOR_create_rectangular_communicator_INC
+#define COMMUNICATOR_create_rectangular_communicator_INC
+
+//! \file create_rectangular_communicator.h
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace COMMUNICATOR
+{
+
+//! \brief Stud text for brief description
+void create_rectangular_communicator ();
+
+
+}; // namespace COMMUNICATOR
+
+#endif // COMMUNICATOR_create_rectangular_communicator_INC
