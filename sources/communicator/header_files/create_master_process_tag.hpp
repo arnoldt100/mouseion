c01f93907b5cd292c629dc67da343b8af93ff387
Arnold Tharrington <arnoldt@ornl.gov>
2024-04-05 17:09:45 -0400

    Transient commit.

---
+++ sources/communicator/header_files/create_master_process_tag.hpp
@@ -0,0 +1,33 @@
+#ifndef COMMUNICATOR_create_master_process_tag_INC
+#define COMMUNICATOR_create_master_process_tag_INC
+
+//! \file create_master_process_tag.hpp
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+#include "MasterProcess.h"
+
+namespace COMMUNICATOR
+{
+
+//! \brief Stud text for brief description
+template< typename T >
+MasterProcess create_master_process_tag ( T const & my_communicator)
+{
+    const bool value = my_communicator->iAmMasterProcess();
+    MasterProcess my_master_process{value};
+    return my_master_process;
+}
+
+}; // namespace COMMUNICATOR
+
+#endif // COMMUNICATOR_create_master_process_tag_INC
