79bb10e77ef0f3fa7e6e816a6613703553e4ac00
Arnold Tharrington <arnoldt@ornl.gov>
2024-04-07 11:05:53 -0400

    Initial commit of new source file.

---
+++ sources/communicator/header_files/create_communicator_size_tag.hpp
@@ -0,0 +1,31 @@
+#ifndef COMMUNICATOR_create_communicator_size_tag_INC
+#define COMMUNICATOR_create_communicator_size_tag_INC
+
+//! \file create_communicator_size_tag.hpp
+
+//--------------------------------------------------------//
+//-------------------- System includes -------------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//-------------------- External Library Files ------------//
+//--------------------------------------------------------//
+
+//--------------------------------------------------------//
+//--------------------- Package includes -----------------//
+//--------------------------------------------------------//
+
+
+namespace COMMUNICATOR
+{
+
+//! \brief Stud text for brief description
+template< <+template_parameters+> >
+void create_communicator_size_tag ( <+argument_list+> )
+{
+        return;
+}
+
+}; // namespace COMMUNICATOR
+
+#endif // COMMUNICATOR_create_communicator_size_tag_INC
